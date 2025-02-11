6638546b2a8b86393263f858b7fd5c13dab5b5d9
Obinna Emmanuel Obi-Akwari <emmanuel0058@gmail.com>
2025-02-11 06:58:56 +0100

    Add TwoAuthors.tsx component with the HTML structure to cite a book with two authors in oscola format

---
+++ src/components/secondarySources/books/TwoAuthors.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+function TwoAuthors() {
+    return(
+        <>
+
+            <form action="">
+                <h3>Book with  two authors</h3>
+                
+                <label htmlFor="">Author's First Name</label> <br />
+                <input type="text" /> <br />
+
+                <label htmlFor="">Followed by their surname</label> <br />
+                <input type="text" /><br />
+
+                <label htmlFor="">2nd Author's First Name</label> <br />
+                <input type="text" /> <br />
+
+                <label htmlFor="">2nd Author's Surname</label> <br />
+                <input type="text" /> <br />
+
+                <label htmlFor="">Title of the Book</label><br />
+                <input type="text" /><br />
+
+                <label htmlFor="">Edition (if not the first)</label><br />
+                <input type="text" /><br />
+
+                <label htmlFor="">The publisher</label><br />
+                <input type="text" /><br />
+
+                <label htmlFor="">the year of publication</label><br />
+                <input type="text" /><br />
+
+                <label htmlFor="">Paragraph Number (e.g 3, if applicable)</label> <br />
+                <input type="text" /> <br />
+
+                <label htmlFor="">Page Number(s) (e.g 61-89, or 61)</label><br />
+                <input type="text" /> <br />
+
+                <label htmlFor="">Copy Citation</label> <br />
+                <textarea name="" id=""></textarea><br />
+
+                <label htmlFor="">Copy Bibliograpy</label> <br />
+                <textarea name="" id=""></textarea>
+            </form>
+        </>
+    )
+}
+
+export default TwoAuthors;
