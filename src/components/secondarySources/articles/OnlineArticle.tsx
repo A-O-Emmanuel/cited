3aa84885d01eadf452f6ff57236cc694f0983c29
Obinna Emmanuel Obi-Akwari <emmanuel0058@gmail.com>
2025-02-12 01:32:11 +0100

    Add OnlineArticle.tsx component with the HTML structure to cite online articles in oscola format

---
+++ src/components/secondarySources/articles/OnlineArticle.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+function OnlineArticle() {
+    return(
+        <>
+            <form action="">
+                <h3>Online Articles</h3>
+
+                <label htmlFor="">Author's First Name</label> <br />
+                <input type="text" /> <br />
+
+                <label htmlFor="">Followed by their surname</label> <br />
+                <input type="text" /><br />
+
+                <label htmlFor="">Does the Article have an author?</label> <br />
+                <label htmlFor=""> yes</label>
+                <input type="radio" /><br />
+
+                <label htmlFor="">No</label>
+                <input type="radio" /> <br />
+
+                <label htmlFor="">Title of the article</label><br />
+                <input type="text" /><br />
+
+                <label htmlFor="">Year</label><br />
+                <input type="text" /><br />
+
+                <label htmlFor="">Volume Number (If applicable)</label><br />
+                <input type="text" /><br />
+
+                <label htmlFor="">Issue Number (If applicable)</label><br />
+                <input type="text" /><br />
+
+                <label htmlFor="">Journal Abbrevation</label><br />
+                <input type="text" /><br />
+
+                <label htmlFor="">First page of article</label><br />
+                <input type="text" /><br />
+
+                <label htmlFor="">URL/web address</label><br />
+                <input type="text" /><br />
+
+                <label htmlFor="">Date accessed</label><br />
+                <input type="date" /><br />
+
+                <label htmlFor="">Copy Citation</label> <br />
+                <textarea name="" id=""></textarea><br />
+
+                <label htmlFor="">Copy Bibliograpy</label> <br />
+                <textarea name="" id=""></textarea>
+            </form>
+        </>
+    )
+}
+
+export default OnlineArticle;
