553a88c0635efc85242fe6c6b71fd3b6b2edd250
Obinna Emmanuel Obi-Akwari <emmanuel0058@gmail.com>
2025-02-13 08:20:29 +0100

    Add X.tsx component with the HTML structure to cite posts on X in oscola format

---
+++ src/components/secondarySources/socialMedia/X.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function Twitter() {
+    return(
+        <>
+            <form action="">
+
+                <h3>Facebook</h3>
+
+                <label htmlFor="">Contributor(s)</label> <br />
+                <input type="text" /><br />
+
+                <label htmlFor="">Short description of post</label> <br />
+                <input type="text" /> <br />
+
+                <label htmlFor="">The name of the facebook page</label> <br />
+                <input type="text" /><br />
+
+                <label htmlFor="">Date of post</label><br />
+                <input type="date" /><br />
+
+                <label htmlFor="">URL/web address of facebook page</label><br />
+                <input type="text" /><br />
+
+                <label htmlFor="">Date you accessed the post</label> <br />
+                <input type="date" /> <br />
+
+                <label htmlFor="">Copy Citation</label> <br />
+                <textarea name="" id=""></textarea><br />
+
+                <label htmlFor="">Copy Bibliograpy</label> <br />
+                <textarea name="" id=""></textarea>
+            </form>
+        </>
+    )
+}
+
+export default Twitter;
