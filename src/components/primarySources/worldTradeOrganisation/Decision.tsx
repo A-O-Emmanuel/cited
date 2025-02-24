3de111b37ef2a7e709d985ad5379e8833f0c7beb
Obinna Emmanuel Obi-Akwari <emmanuel0058@gmail.com>
2025-02-24 18:57:30 +0100

    add Decision.tsx component with the HTML structure to cite world trade organisation decisions in oscola format

---
+++ src/components/primarySources/worldTradeOrganisation/Decision.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Decision() {
+    return(
+        <>
+
+            <form action="">
+                <h3>World Trade Organisation decisions</h3>
+
+                <label htmlFor="">Title</label> <br />
+                <input type="text" /> <br />
+
+                <label htmlFor="">date of decision</label> <br />
+                <input type="date" /><br />
+
+                <label htmlFor="">World Trade Organisation catalog number</label><br />
+                <input type="text" /><br />
+
+                <label htmlFor="">Copy Citation</label> <br />
+                <textarea name="" id=""></textarea><br />
+
+                <label htmlFor="">Table of International Treaties and Conventions</label> <br />
+                <textarea name="" id=""></textarea>
+            </form>
+        </>
+    )
+}
+
+export default Decision;
